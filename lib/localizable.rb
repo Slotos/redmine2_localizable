c3acce6c52a934a77384db79360dc4b584db13f1
slotos <slotos@faust.(none)>
2012-11-13 12:57:35 +0000

    Another fix for plugin naming

--- lib/localizable.rb
+++ lib/localizable.rb
@@ -2,12 +2,12 @@ module Localizable
 
   def self.localize(type, id, name)
     value = name
-    Setting["plugin_localizable"] and
-      Setting["plugin_localizable"]["locales"] and
-      Setting["plugin_localizable"]["locales"][type] and
-      Setting["plugin_localizable"]["locales"][type][id.to_s] and
-      not Setting["plugin_localizable"]["locales"][type][id.to_s][User.current.language.to_s].blank? and
-      value = Setting["plugin_localizable"]["locales"][type][id.to_s][User.current.language.to_s]
+    Setting["plugin_redmine2_localizable"] and
+      Setting["plugin_redmine2_localizable"]["locales"] and
+      Setting["plugin_redmine2_localizable"]["locales"][type] and
+      Setting["plugin_redmine2_localizable"]["locales"][type][id.to_s] and
+      not Setting["plugin_redmine2_localizable"]["locales"][type][id.to_s][User.current.language.to_s].blank? and
+      value = Setting["plugin_redmine2_localizable"]["locales"][type][id.to_s][User.current.language.to_s]
     return(value)
   end
 
