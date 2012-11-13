71aadaf55d5aabc1e2e622ef239b92ce43cd46bb
slotos <slotos@faust.(none)>
2012-11-13 12:50:40 +0000

    Applying previously lost negations

--- lib/localizable.rb
+++ lib/localizable.rb
@@ -2,11 +2,11 @@ module Localizable
 
   def self.localize(type, id, name)
     value = name
-    Setting["plugin_localizable"].nil? and
-      Setting["plugin_localizable"]["locales"].nil? and
-      Setting["plugin_localizable"]["locales"][type].nil? and
-      Setting["plugin_localizable"]["locales"][type][id.to_s].nil? and
-      Setting["plugin_localizable"]["locales"][type][id.to_s][User.current.language.to_s].blank? and
+    Setting["plugin_localizable"] and
+      Setting["plugin_localizable"]["locales"] and
+      Setting["plugin_localizable"]["locales"][type] and
+      Setting["plugin_localizable"]["locales"][type][id.to_s] and
+      not Setting["plugin_localizable"]["locales"][type][id.to_s][User.current.language.to_s].blank? and
       value = Setting["plugin_localizable"]["locales"][type][id.to_s][User.current.language.to_s]
     return(value)
   end
